d9edcf44c2333fa82553748e6917ada7d78dc663
wan01905 <wan01905@umn.edu>
2026-05-12 17:15:08 +0300

    Fix financial detail build import

--- app/src/main/java/com/stockwatchdog/app/ui/detail/TickerDetailScreen.kt
+++ app/src/main/java/com/stockwatchdog/app/ui/detail/TickerDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.stockwatchdog.app.ui.detail
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.Arrangement
